c8ad397b53fa71b7881c06045e5cd5d7196d6b40
Nirav <niravnvk@gmail.com>
2015-03-12 19:50:01 +0530

    as

---
+++ app/src/main/java/com/nkdroid/fransandre/HomeActivity.java
@@ -0,0 +1,39 @@
+package com.nkdroid.fransandre;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SlidingDrawer;
+import android.widget.Toast;
+
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HomeActivity extends ActionBarActivity  {
+
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_home);
+
+    }
+
+}
